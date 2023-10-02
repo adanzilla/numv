fdf1567d1de8498b42416ea9e24478487ce99738
Adan Serrano <adan.serrano@gmail.com>
2023-10-01 22:03:11 -0600

    Se agrega la stackedbar de top 10

--- maqueta/js/main.js
+++ maqueta/js/main.js
@@ -1,5 +1,92 @@
 jQuery(document).ready(function($) {
 
+
+	if ( jQuery('#top10-estados-mas-incidentes') ) {
+		const data = {
+			labels: [
+			'Ciudad de México',
+		  	'Estado de México',
+		  	'Nayarit',
+		  	'Michoacán',
+		  	'Guanajuato',
+		  	'Jalisco',
+		  	'Durango',
+		  	'Sinaloa',
+		  	'Puebla',
+		  	'Queretaro'
+		  	],
+		  	datasets: [
+		  		{
+			  		label: [
+			  			"Peatones"
+			  		],
+			  		data: [
+			  			2,3,4,5,6,7,3,4,2,3
+			  		],
+			  		backgroundColor: '#FFCC14'
+			  	},
+			  	{
+			  		label: [
+			  			"Motociclistas"
+			  		],
+			  		data: [
+			  			1,2,3,1,2,3,1,2,3,1
+			  		],
+			  		backgroundColor: '#64D3C3'
+			  	},
+			  	{
+			  		label: [
+			  			"Ciclistas"
+			  		],
+			  		data: [
+			  			1,4,2,6,2,7,2,5,2,1
+			  		],
+			  		backgroundColor: '#A073C3'
+			  	},
+			]
+		};
+
+		const config = {
+			type: 'bar',
+			data: data,
+			options: {
+				
+				plugins: {
+					legend : {
+						align: 'start',
+						labels : {
+							color: '#000000',
+							padding : 10,
+							usePointStyle: true,
+							font: {
+								size: 12
+							},
+							boxHeight: 100
+						}
+					}
+				},
+			    
+				responsive: false,
+				legend: {
+					position: 'right' 
+				},
+				scales: {
+				      x: {
+				        stacked: true,
+				      },
+				      y: {
+				        stacked: true
+				      }
+				    }
+			}
+		};
+
+		 const myChart = new Chart( 
+			 document.getElementById('top10-estados-mas-incidentes'),
+			 config 
+		);	
+	}
+
 	jQuery('#scroll-top').on('click', function(event) {
 		event.preventDefault();
 		$('html,body').animate({
