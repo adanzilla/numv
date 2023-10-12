7d816123c5816a30488ad7035224b6cd2f916674
Adan Serrano <adan.serrano@gmail.com>
2023-10-12 17:41:06 -0600

    Ajuste al FOOTER

--- wp-content/themes/numv/page-templates/inicio.php
+++ wp-content/themes/numv/page-templates/inicio.php
@@ -531,50 +531,47 @@ get_header(); ?>
 </section>
 
 <section class="my-5 pb-md-5" id="nuestra-historia">
-    <div class="row no-gutters">
-        <div class="col-11 col-sm-10 col-lg-11 mx-auto">
-    
-            <div class="row no-gutters">
-                <div class="col-12 col-md-7 left-column pr-md-5">
-                    
-                    <h2 class="text-left mb-4">Nuestra historia</h2>
-                    
-                    <p>Ni Una Muerte Vial surge en noviembre de 2018, a partir del atropellamiento de Manuel Vara, especialista en movilidad, en Puebla. Decidimos que para honrar la memoria de las víctimas de siniestros de tránsito, nos abocaríamos a reconocer y visibilizar a los peatones y ciclistas atropellados en el país. Eventualmente decidimos integrar datos de motociclistas.</p>
-                    
-                    <img src="<?php echo get_template_directory_uri(); ?>/img/nuestra-historia-logo-1.svg" class="d-inline-block mr-3">
-                    <img src="<?php echo get_template_directory_uri(); ?>/img/nuestra-historia-logo-2.svg" class="d-inline-block mr-3">
-                    <img src="<?php echo get_template_directory_uri(); ?>/img/nuestra-historia-logo-3.svg" class="d-inline-block mr-3">
-                </div>
-                <div class="col-12 col-md-5 right-column">
-                    <div class="row no-gutters">
-                        <div class="col-12 text-center">
-                            <img src="<?php echo get_template_directory_uri(); ?>/img/nuestra-historia-hero.png" class="img-fluid">
-                        </div>
-                    </div>
+    <div class="container">
 
-                    <div class="row no-gutters caption-box">
-                        <div class="col-10 mx-auto caption-body p-4">
-
-                            <div class="row no-gutters">
-                                <div class="col-4 d-flex flex-column pr-md-3">
-                                    <p class="years my-auto text-center">
-                                        +4
-                                        <span>Años de datos</span>
-                                    </p>
-                                </div>
-                                <div class="col-8 d-flex flex-column pl-2 pl-md-0">
-                                    <p class="my-auto">
-                                        Luchando y esperando a que nunca más muera nadie en las calles y para que algún día ese dato sea CERO. 
-                                    </p>
-                                </div>
-                            </div>
-                            
-                        </div>
-                    </div>
-
-                </div>
+        <div class="row no-gutters">
+            <div class="col-12 col-md-7 left-column pr-md-5">
+                
+                <h2 class="text-left mb-4">Nuestra historia</h2>
+                
+                <p>Ni Una Muerte Vial surge en noviembre de 2018, a partir del atropellamiento de Manuel Vara, especialista en movilidad, en Puebla. Decidimos que para honrar la memoria de las víctimas de siniestros de tránsito, nos abocaríamos a reconocer y visibilizar a los peatones y ciclistas atropellados en el país. Eventualmente decidimos integrar datos de motociclistas.</p>
+                
+                <img src="<?php echo get_template_directory_uri(); ?>/img/nuestra-historia-logo-1.svg" class="logo d-inline-block mr-3">
+                <img src="<?php echo get_template_directory_uri(); ?>/img/nuestra-historia-logo-2.svg" class="logo d-inline-block mr-3">
+                <img src="<?php echo get_template_directory_uri(); ?>/img/nuestra-historia-logo-3.svg" class="logo d-inline-block mr-3">
             </div>
+            <div class="col-12 col-md-5 right-column mt-5 mt-md-0">
+                <div class="row no-gutters">
+                    <div class="col-12 text-center">
+                        <img src="<?php echo get_template_directory_uri(); ?>/img/nuestra-historia-hero.png" class="img-fluid">
+                    </div>
+                </div>
 
+                <div class="row no-gutters caption-box">
+                    <div class="col-10 mx-auto caption-body p-4">
+
+                        <div class="row no-gutters">
+                            <div class="col-4 d-flex flex-column pr-md-3">
+                                <p class="years my-auto text-center">
+                                    +4
+                                    <span>Años de datos</span>
+                                </p>
+                            </div>
+                            <div class="col-8 d-flex flex-column pl-2 pl-md-0">
+                                <p class="my-auto">
+                                    Luchando y esperando a que nunca más muera nadie en las calles y para que algún día ese dato sea CERO. 
+                                </p>
+                            </div>
+                        </div>
+                        
+                    </div>
+                </div>
+
+            </div>
         </div>
     </div>
 </section>
