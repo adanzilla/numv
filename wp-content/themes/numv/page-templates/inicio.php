b733fc4241014e39254943f6d16e6e49df9cc739
Adan Serrano <adan.serrano@gmail.com>
2023-10-12 17:28:09 -0600

    Ajustes a QUIENESOMOS

--- wp-content/themes/numv/page-templates/inicio.php
+++ wp-content/themes/numv/page-templates/inicio.php
@@ -414,116 +414,118 @@ get_header(); ?>
 </section>
 
  <section class="my-0 my-md-5 p" id="quienes-somos">
-    <div class="row no-gutters">
-        <div class="col-12 col-sm-10 col-lg-9 mx-auto">
-    
-            <div class="row no-gutters">
-                <div class="col-12">
-                    
-                    <h2 class="text-center">Quiénes somos</h2>
-                    <p class="text-center">Céntrico es un equipo de especialistas apasionadas de las ciudades, la movilidad, y la seguridad vial. Non enfocamos en la implementación de proyectos de movilidad sustentable, así como en el desarrollo de políticas públicas que eleven las condiciones de la movilidad en el país.</p>
-                    <p class="text-center">Si deseas saber más sobre Céntrico, visita <a href="https://www.centrico.mx">www.centrico.mx</a></p>
+    <div class="container for-mobile">
+        <div class="row no-gutters">
+            <div class="col-12 col-sm-10 col-lg-9 mx-auto">
+        
+                <div class="row no-gutters">
+                    <div class="col-12">
+                        
+                        <h2 class="text-center">Quiénes somos</h2>
+                        <p class="text-center">Céntrico es un equipo de especialistas apasionadas de las ciudades, la movilidad, y la seguridad vial. Non enfocamos en la implementación de proyectos de movilidad sustentable, así como en el desarrollo de políticas públicas que eleven las condiciones de la movilidad en el país.</p>
+                        <p class="text-center">Si deseas saber más sobre Céntrico, visita <a href="https://www.centrico.mx">www.centrico.mx</a></p>
+                    </div>
                 </div>
+
             </div>
 
-        </div>
+            <div class="col-12 col-sm-10 col-lg-12 mx-auto">
 
-        <div class="col-12 col-sm-10 col-lg-12 mx-auto">
+                <div class="row no-gutters my-5">
+                    <div class="col-12">
+                        <div class="swiper mySwiperTeam">
+                           
+                            <div class="swiper-wrapper">
+                                <div class="swiper-slide">
+                                    <div class="card py-4 px-3">
+                                        <p class="name">
+                                            Alejandra
+                                            Leal
+                                        </p>
+                                        <p class="position">
+                                            Codirectora<br>
+                                            Coordinadora política pública
+                                        </p>
+                                        <img src="<?php echo get_template_directory_uri(); ?>/img/alejandra-leal.png" alt="" class="img-fluid">
+                                    </div>
+                                </div>
+                                <div class="swiper-slide">
+                                    <div class="card py-4 px-3">
+                                        <p class="name">
+                                            Xavier
+                                            Treviño
+                                        </p>
+                                        <p class="position">
+                                            Co-director<br>
+                                            Coordinador análisis de datos
+                                        </p>
+                                        <img src="<?php echo get_template_directory_uri(); ?>/img/xavier-trevino.png" alt="" class="img-fluid">
+                                    </div>
+                                </div>
+                                <div class="swiper-slide">
+                                    <div class="card py-4 px-3">
+                                        <p class="name">
+                                            Patricio
+                                            Ruiz
+                                        </p>
+                                        <p class="position">
+                                            Coordinador de diseño
+                                        </p>
+                                        <img src="<?php echo get_template_directory_uri(); ?>/img/patricio-ruiz.png" alt="" class="img-fluid">
+                                    </div>
+                                </div>
+                                <div class="swiper-slide">
+                                    <div class="card py-4 px-3">
+                                        <p class="name">
+                                            Héctor
+                                            Garrido
+                                        </p>
+                                        <p class="position">
+                                            Consultor de datos
+                                        </p>
+                                        <img src="<?php echo get_template_directory_uri(); ?>/img/hector-garrido.png" alt="" class="img-fluid">
+                                    </div>
+                                </div>
+                                <div class="swiper-slide">
+                                    <div class="card py-4 px-3">
+                                        <p class="name">
+                                            Norber
+                                            Sánchez
+                                        </p>
+                                        <p class="position">
+                                            Datos
+                                        </p>
+                                        <img src="<?php echo get_template_directory_uri(); ?>/img/norber-sanchez.png" alt="" class="img-fluid">
+                                    </div>
+                                </div>
+                                <div class="swiper-slide">
+                                    <div class="card py-4 px-3">
+                                        <p class="name">
+                                            Norber
+                                            Sánchez
+                                        </p>
+                                        <p class="position">
+                                            Datos
+                                        </p>
+                                        <img src="<?php echo get_template_directory_uri(); ?>/img/norber-sanchez.png" alt="" class="img-fluid">
+                                    </div>
+                                </div>
+                            </div>
 
-            <div class="row no-gutters my-5">
-                <div class="col-12">
-                    <div class="swiper mySwiperTeam">
-                       
-                        <div class="swiper-wrapper">
-                            <div class="swiper-slide">
-                                <div class="card py-4 px-3">
-                                    <p class="name">
-                                        Alejandra
-                                        Leal
-                                    </p>
-                                    <p class="position">
-                                        Codirectora<br>
-                                        Coordinadora política pública
-                                    </p>
-                                    <img src="<?php echo get_template_directory_uri(); ?>/img/alejandra-leal.png" alt="" class="img-fluid">
-                                </div>
-                            </div>
-                            <div class="swiper-slide">
-                                <div class="card py-4 px-3">
-                                    <p class="name">
-                                        Xavier
-                                        Treviño
-                                    </p>
-                                    <p class="position">
-                                        Co-director<br>
-                                        Coordinador análisis de datos
-                                    </p>
-                                    <img src="<?php echo get_template_directory_uri(); ?>/img/xavier-trevino.png" alt="" class="img-fluid">
-                                </div>
-                            </div>
-                            <div class="swiper-slide">
-                                <div class="card py-4 px-3">
-                                    <p class="name">
-                                        Patricio
-                                        Ruiz
-                                    </p>
-                                    <p class="position">
-                                        Coordinador de diseño
-                                    </p>
-                                    <img src="<?php echo get_template_directory_uri(); ?>/img/patricio-ruiz.png" alt="" class="img-fluid">
-                                </div>
-                            </div>
-                            <div class="swiper-slide">
-                                <div class="card py-4 px-3">
-                                    <p class="name">
-                                        Héctor
-                                        Garrido
-                                    </p>
-                                    <p class="position">
-                                        Consultor de datos
-                                    </p>
-                                    <img src="<?php echo get_template_directory_uri(); ?>/img/hector-garrido.png" alt="" class="img-fluid">
-                                </div>
-                            </div>
-                            <div class="swiper-slide">
-                                <div class="card py-4 px-3">
-                                    <p class="name">
-                                        Norber
-                                        Sánchez
-                                    </p>
-                                    <p class="position">
-                                        Datos
-                                    </p>
-                                    <img src="<?php echo get_template_directory_uri(); ?>/img/norber-sanchez.png" alt="" class="img-fluid">
-                                </div>
-                            </div>
-                            <div class="swiper-slide">
-                                <div class="card py-4 px-3">
-                                    <p class="name">
-                                        Norber
-                                        Sánchez
-                                    </p>
-                                    <p class="position">
-                                        Datos
-                                    </p>
-                                    <img src="<?php echo get_template_directory_uri(); ?>/img/norber-sanchez.png" alt="" class="img-fluid">
-                                </div>
-                            </div>
+                           
+
                         </div>
-
-                       
-
+                        <div class="swiper-button-prev">
+                            <img src="<?php echo get_template_directory_uri(); ?>/img/rounded-left-arrow-dark.svg" alt="">
+                        </div>
+                        <div class="swiper-button-next">
+                            <img src="<?php echo get_template_directory_uri(); ?>/img/rounded-right-arrow-dark.svg" alt="">
+                        </div>
                     </div>
-                    <div class="swiper-button-prev">
-                        <img src="<?php echo get_template_directory_uri(); ?>/img/rounded-left-arrow-dark.svg" alt="">
-                    </div>
-                    <div class="swiper-button-next">
-                        <img src="<?php echo get_template_directory_uri(); ?>/img/rounded-right-arrow-dark.svg" alt="">
-                    </div>
+                    
                 </div>
-                
-            </div>
 
+            </div>
         </div>
     </div>
 </section>
