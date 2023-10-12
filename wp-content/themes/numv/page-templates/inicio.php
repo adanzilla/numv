78c114b272362ccf3b0726de6a90062273cdd335
Adan Serrano <adan.serrano@gmail.com>
2023-10-12 17:20:29 -0600

    Ajustes en REPORTES

--- wp-content/themes/numv/page-templates/inicio.php
+++ wp-content/themes/numv/page-templates/inicio.php
@@ -285,131 +285,131 @@ get_header(); ?>
 </section>
 
 <section class="my-5 p" id="reportes">
-    <div class="row no-gutters">
-        <div class="col-12 col-sm-10 col-lg-9 mx-auto">
-    
-            <div class="row no-gutters">
-                <div class="col-12">
-                    
-                    <h2 class="text-center">Reportes anuales</h2>
-                    <p class="text-center">Compilación de resultados y datos relevantes de años previos</p>
+    <div class="container">
+        
+        
+        <div class="row no-gutters">
+            <div class="col-12">
                 
-                </div>
-            </div>
-
-            <div class="row no-gutters my-5">
-                <div class="col-11 col-md-6 mx-auto">
-                    
-                    <div class="row no-gutters">
-                        <div class="col-12">
-                            <h4>Últimas publicaciones</h4>
-                        </div>
-                    </div>
-
-                    <div class="row no-gutters">
-                        <div class="col-12 col-md-6 pr-1">
-                            <div class="card p-3">
-                                <img src="<?php echo get_template_directory_uri(); ?>/img/thumbnail.png" alt="" class="mb-3">
-                                <p class="tag mb-1">Datos 2020-2021</p>
-                                <p class="title">Las 150 vías más peligrosas</p>
-                                <p class="extract">
-                                    El desafío de convertir zonas de riesgo en entornos seguros
-                                </p>
-                                <p>
-                                    <a href="https://niunamuertevial.mx/docs/las150mas.pdf" class="btn btn-block btn-primary mt-3" download="">
-                                        Consultar reporte <img src="<?php echo get_template_directory_uri(); ?>/img/right-arrow.svg" alt="">
-                                    </a>
-                                </p>
-                            </div>      
-                        </div>
-                        <div class="col-12 col-md-6 pl-1">
-                            <div class="card p-3">
-                                <img src="<?php echo get_template_directory_uri(); ?>/img/thumbnail.png" alt="" class="mb-3">
-                                <p class="tag mb-1">Datos 2020-2021</p>
-                                <p class="title">Reporte anual 2019-2020</p>
-                                <p class="extract">
-                                    El desafío de convertir zonas de riesgo en entornos seguros
-                                </p>
-                                <p>
-                                    <a href="https://niunamuertevial.mx/wp-content/uploads/2020/11/informe-NUMV-2019.pdf" class="btn btn-block btn-primary mt-3" download="">
-                                        Consultar reporte <img src="<?php echo get_template_directory_uri(); ?>/img/right-arrow.svg" alt="">
-                                    </a>
-                                </p>
-                            </div>      
-                        </div>
-                    </div>
-                </div>
-                <div class="col-11 col-md-6 pl-md-5 mx-auto mt-5 mt-md-0">
-                    
-                    <div class="row no-gutters">
-                        <div class="col-12">
-                            <h4>Recolección de datos y análisis anual</h4>
-                        </div>
-                    </div>
-
-                    
-                        <div class="row no-gutters bordered">
-                            <div class="col-10 d-flex flex-column">
-                                <p class="title my-auto">
-                                    <a href="javascript:void(0);">
-                                        Reporte anual 2019-2020
-                                        <span class="d-block mt-2">Texto sobre</span>
-                                    </a>
-                                </p>
-                            </div>
-                            <div class="col-2 d-flex flex-column text-right">
-                                <a href="javascript:(0);" class="my-auto">
-                                    <img src="<?php echo get_template_directory_uri(); ?>/img/right-arrow-dark.svg" alt="">
-                                </a>
-                            </div>
-                        </div>
-
-                        <div class="row no-gutters bordered">
-                            <div class="col-10 d-flex flex-column">
-                                <p class="title my-auto">
-                                    <a href="javascript:void(0);">
-                                        Reporte anual 2019-2020
-                                        <span class="d-block mt-2">Texto sobre</span>
-                                    </a>
-                                </p>
-                            </div>
-                            <div class="col-2 d-flex flex-column text-right">
-                                <a href="javascript:(0);" class="my-auto">
-                                    <img src="<?php echo get_template_directory_uri(); ?>/img/right-arrow-dark.svg" alt="">
-                                </a>
-                            </div>
-                        </div>
-
-                        <div class="row no-gutters bordered">
-                            <div class="col-10 d-flex flex-column">
-                                <p class="title my-auto">
-                                    <a href="javascript:void(0);">
-                                        Reporte anual 2019-2020
-                                        <span class="d-block mt-2">Texto sobre</span>
-                                    </a>
-                                </p>
-                            </div>
-                            <div class="col-2 d-flex flex-column text-right">
-                                <a href="javascript:(0);" class="my-auto">
-                                    <img src="<?php echo get_template_directory_uri(); ?>/img/right-arrow-dark.svg" alt="">
-                                </a>
-                            </div>
-                        </div>
-
-
-                        <div class="row no-gutters ver-todos">
-                            <div class="col-4 d-flex flex-column text-right pr-0 ml-auto">
-                                <a href="" class="btn btn-block btn-primary mt-3 transparent">
-                                    Ver todo <img src="<?php echo get_template_directory_uri(); ?>/img/right-arrow-primary.svg" alt="">
-                                </a>
-                            </div>
-                        </div>
-
-                    </div>
-
-                </div>
+                <h2 class="text-center">Reportes anuales</h2>
+                <p class="text-center">Compilación de resultados y datos relevantes de años previos</p>
+            
             </div>
         </div>
+
+        <div class="row no-gutters my-5">
+            <div class="col-12 col-md-6 mx-auto">
+                
+                <div class="row no-gutters">
+                    <div class="col-12">
+                        <h4>Últimas publicaciones</h4>
+                    </div>
+                </div>
+
+                <div class="row no-gutters">
+                    <div class="col-12 col-md-6 pr-1">
+                        <div class="card p-3">
+                            <img src="<?php echo get_template_directory_uri(); ?>/img/thumbnail.png" alt="" class="mb-3">
+                            <p class="tag mb-1">Datos 2020-2021</p>
+                            <p class="title">Las 150 vías más peligrosas</p>
+                            <p class="extract">
+                                El desafío de convertir zonas de riesgo en entornos seguros
+                            </p>
+                            <p>
+                                <a href="https://niunamuertevial.mx/docs/las150mas.pdf" class="btn btn-block btn-primary mt-3" download="">
+                                    Consultar reporte <img src="<?php echo get_template_directory_uri(); ?>/img/right-arrow.svg" alt="">
+                                </a>
+                            </p>
+                        </div>      
+                    </div>
+                    <div class="col-12 col-md-6 pl-1 mt-3 mt-md-0">
+                        <div class="card p-3">
+                            <img src="<?php echo get_template_directory_uri(); ?>/img/thumbnail.png" alt="" class="mb-3">
+                            <p class="tag mb-1">Datos 2020-2021</p>
+                            <p class="title">Reporte anual 2019-2020</p>
+                            <p class="extract">
+                                El desafío de convertir zonas de riesgo en entornos seguros
+                            </p>
+                            <p>
+                                <a href="https://niunamuertevial.mx/wp-content/uploads/2020/11/informe-NUMV-2019.pdf" class="btn btn-block btn-primary mt-3" download="">
+                                    Consultar reporte <img src="<?php echo get_template_directory_uri(); ?>/img/right-arrow.svg" alt="">
+                                </a>
+                            </p>
+                        </div>      
+                    </div>
+                </div>
+            </div>
+            <div class="col-11 col-md-6 pl-md-5 mx-auto mt-5 mt-md-0">
+                
+                <div class="row no-gutters">
+                    <div class="col-12">
+                        <h4>Recolección de datos y análisis anual</h4>
+                    </div>
+                </div>
+
+                
+                    <div class="row no-gutters bordered">
+                        <div class="col-10 d-flex flex-column">
+                            <p class="title my-auto">
+                                <a href="javascript:void(0);">
+                                    Reporte anual 2019-2020
+                                    <span class="d-block mt-2">Texto sobre</span>
+                                </a>
+                            </p>
+                        </div>
+                        <div class="col-2 d-flex flex-column text-right">
+                            <a href="javascript:(0);" class="my-auto">
+                                <img src="<?php echo get_template_directory_uri(); ?>/img/right-arrow-dark.svg" alt="">
+                            </a>
+                        </div>
+                    </div>
+
+                    <div class="row no-gutters bordered">
+                        <div class="col-10 d-flex flex-column">
+                            <p class="title my-auto">
+                                <a href="javascript:void(0);">
+                                    Reporte anual 2019-2020
+                                    <span class="d-block mt-2">Texto sobre</span>
+                                </a>
+                            </p>
+                        </div>
+                        <div class="col-2 d-flex flex-column text-right">
+                            <a href="javascript:(0);" class="my-auto">
+                                <img src="<?php echo get_template_directory_uri(); ?>/img/right-arrow-dark.svg" alt="">
+                            </a>
+                        </div>
+                    </div>
+
+                    <div class="row no-gutters bordered">
+                        <div class="col-10 d-flex flex-column">
+                            <p class="title my-auto">
+                                <a href="javascript:void(0);">
+                                    Reporte anual 2019-2020
+                                    <span class="d-block mt-2">Texto sobre</span>
+                                </a>
+                            </p>
+                        </div>
+                        <div class="col-2 d-flex flex-column text-right">
+                            <a href="javascript:(0);" class="my-auto">
+                                <img src="<?php echo get_template_directory_uri(); ?>/img/right-arrow-dark.svg" alt="">
+                            </a>
+                        </div>
+                    </div>
+
+
+                    <div class="row no-gutters ver-todos">
+                        <div class="col-4 d-flex flex-column text-right pr-0 ml-auto">
+                            <a href="" class="btn btn-block btn-primary mt-3 transparent">
+                                Ver todo <img src="<?php echo get_template_directory_uri(); ?>/img/right-arrow-primary.svg" alt="">
+                            </a>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+            
     </div>
 </section>
 
