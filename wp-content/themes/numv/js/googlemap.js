a497920838302a3a2d63af6e2116ecf5657e6e39
Adan Serrano <adan.serrano@gmail.com>
2023-10-17 03:18:36 -0600

    Se ajusta la tarjetita del mapa

--- wp-content/themes/numv/js/googlemap.js
+++ wp-content/themes/numv/js/googlemap.js
@@ -51,10 +51,10 @@ function initMap() {
 				const contentString = '<div class="row no-gutters info-window">'+
 									    '<div class="col-12">'+
 									        '<p class="title"><img src="'+ googlemap_data.template+location['icon'] +'"> <span style="font-weight: 600;">'+ category +'</span></p>'+
-									        '<p><span style="font-weight: 600;">Usuario:</span> Conductor</p>'+
+									        '<p><span style="font-weight: 600;">Usuario:</span> '+ location['usuario'] +'</p>'+
 									        '<p><span style="font-weight: 600;">Sexo:</span> '+ location['genero'] +'</p>'+
 									        '<p><span style="font-weight: 600;">Edad:</span> '+ location['edad'] +' años</p>'+
-									        '<p><span style="font-weight: 600;">Vehículo:</span> </p>'+
+									        '<p><span style="font-weight: 600;">Vehículo:</span> '+ location['vehiculo'] +'</p>'+
 									        '<hr>'+
 									        '<p><span style="font-weight: 600;">Cuándo</span></p>'+
 									        '<p><span style="font-weight: 600;">Fecha:</span> '+ location['fecha'] +'</p>'+
